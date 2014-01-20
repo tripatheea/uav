22f37532376c4a2aed67d75f1f65adfc5dd06320
Aashish Tripathee <aashisht@mit.edu>
2014-01-20 10:58:39 -0500

    Aerial imaging among other things.

--- smile/tiles/divide-and-win.py
+++ smile/tiles/divide-and-win.py
@@ -1,4 +1,5 @@
 import glob, os, math
+from decimal import *
 
 images = glob.glob('*.png')
 
@@ -7,10 +8,10 @@ for img in images:
     first = parts[0]
     second = parts[1].split(',')[0]
     second = second.split('.')[0] + '.' + second.split('.')[1]
-    first = float(first)*3600.0
-    second = float(second)*3600.0
-    first = math.floor(first);
-    second = math.floor(second);
+    getcontext().prec = 6
+    first = float(first);
+    second = float(second);
+
     new = str(first) + ',' + str(second) + '.png'
-    os.rename(img, new)
+    #os.rename(img, new)
     print str(new) + ';',
